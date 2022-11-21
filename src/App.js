e1ada6da51833cc84830b1dadd8bc292e02d7cb3
VedantKale08 <104070347+VedantKale08@users.noreply.github.com>
2022-11-21 12:47:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState('');
+  const [thing, addThings] = useState([]);
+
+  const handleThings = (e) => {
+    e.preventDefault();
+    if(todo !== ''){
+      localStorage.setItem("todos",JSON.stringify(thing))
+      addThings([...thing, todo])
+      setTodo('')
+    }
+    else{
+      alert("Please Enter Input")
+    }
+    
+  }
+
+  const remove = (index) => {
+    let newThings = thing
+    newThings.splice(index,1)
+    addThings([...newThings])
+  }
+
   return (
+    <div className="main">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleThings}>
+        <input type="text" placeholder="Aad New..." name='todo' value={todo} onChange={(e) => setTodo(e.target.value)} />
+        <button className='button'>Add</button>
+      </form>
+      <div className="map">
+        <div className="mapp">
+        {
+          thing.length !== 0 ?
+          thing.map((i,key)=>{
+            return <div className='item' key={key}>
+              <div style={{flex:"1"}}>{i}</div>
+              <div style={{textAlign:"end", flex:"1"}}> <button className='remove' onClick={()=>remove(key)}>X</button></div>
+            </div>
+          })
+           :
+           <h2>Add Things</h2>
+        }
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
